72adbf4b5ff7d74964f777c2c7c4662828182e81
Glenn-IT <glenard2308@gmail.com>
2026-06-28 20:34:12 +0800

    feat: implement v1.01 - unlock Dashboard

--- components/under-construction.php
+++ components/under-construction.php
@@ -1,5 +1,5 @@
 <?php
-define('CURRENT_VERSION', 'v1.00');
+define('CURRENT_VERSION', 'v1.01');
 
 require_once __DIR__ . '/../config.php';
 if (session_status() === PHP_SESSION_NONE) session_start();
