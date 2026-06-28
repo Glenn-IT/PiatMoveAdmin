ea19d9a5ef101becd77e60ff43ebbbb72cd00f69
Glenn-IT <glenard2308@gmail.com>
2026-06-28 20:35:01 +0800

    feat: implement v1.04 - unlock Bookings

--- components/under-construction.php
+++ components/under-construction.php
@@ -1,5 +1,5 @@
 <?php
-define('CURRENT_VERSION', 'v1.03');
+define('CURRENT_VERSION', 'v1.04');
 
 require_once __DIR__ . '/../config.php';
 if (session_status() === PHP_SESSION_NONE) session_start();
